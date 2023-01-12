2668dc76cb50816f3aa7cc7dd5aa676848e72b4a
Demon673 <liuxingcan112@qq.com>
2023-01-12 11:41:39 +0800

    v0.1.2

--- src/console.ts
+++ src/console.ts
@@ -4,12 +4,12 @@ type ConsoleType = typeof console;
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface Console extends ConsoleType {}
+  interface Console extends ConsoleType { }
 
   // eslint-disable-next-line vars-on-top, no-var
   var console: Console;
 }
 
 // eslint-disable-next-line no-new-func
-const global: typeof globalThis = new Function('return this')();
+const global: typeof globalThis = (() => this as any)();
 global.console = console;
